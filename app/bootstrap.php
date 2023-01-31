dd8e80db7ef249fb24d2755ff13c6eaa784b38f6
achchaimae <achchaimae1@gmail.com>
2023-01-31 11:55:49 +0100

    declare helpers 🔥

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -2,6 +2,10 @@
   // Load Config
   require_once 'config/config.php';
 
+  // Load Helpers
+  require_once 'helpers/redirect_helper.php';
+  require_once 'helpers/session_helper.php';
+
   // Autoload Core Libraries
   spl_autoload_register(function($className){
     require_once 'libraries/' . $className . '.php';
