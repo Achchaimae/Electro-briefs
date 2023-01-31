45c6d0cc876cff307dc8d8ee08d4b74b4836829a
achchaimae <achchaimae1@gmail.com>
2023-01-31 11:56:03 +0100

    configuration ⛏

--- app/config/config.php
+++ app/config/config.php
@@ -1,9 +1,9 @@
 <?php
   // DB Params
   define('DB_HOST', 'localhost');
-  define('DB_USER', 'electro-briefs');
+  define('DB_USER', 'root');
   define('DB_PASS', '');
-  define('DB_NAME', 'root');
+  define('DB_NAME', 'electo_maroc');
 
   // App Root
   define('APPROOT', dirname(dirname(__FILE__)));
