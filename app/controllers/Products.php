6eeb409b2be2f02299b8ce2dc8f1301620c96cd9
achchaimae <achchaimae1@gmail.com>
2023-02-05 20:36:13 +0100

    product logic

--- app/controllers/Products.php
+++ app/controllers/Products.php
@@ -11,6 +11,13 @@
             ];
             $this->view('pages/admin/products',$data);
         }
+        public function user(){
+            $products = $this->productmodels->List();
+            $data = [
+                'products' => $products
+            ];
+            $this->view('pages/product',$data);
+        }
 
         public function add(){
             if($_SERVER['REQUEST_METHOD']=='POST'){
@@ -18,17 +25,19 @@
                 $imgTmp = $_FILES['image']['tmp_name'];
                 move_uploaded_file($imgTmp, 'img/upload/' . $imgName);
                 $data= [
-                     
-                        'date_envoi' => $_POST['date_envoi'],
-                        'date_livraison' => $_POST['date_livraison'],
-                        ' client_id' => $_POST[' client_id'],
-                        'prix_unitaire' => $_POST['prix_unitaire'],
-                        'prix_final' => $_POST['prix_final'],
-                        'prix_offre' => $_POST['prix_offre'],
-                        'quantite' => $_POST['quantite'],
-                        'description' => $_POST['description'],
-                        'photo' => $_FILES['photo'],
-                        'categorie_id' => $_POST['categorie_id'],
+                    //  ref	libelle	code_barre	prix_achat	prix_final	prix_offre	quantite	description	image	categorie_id	
+
+                    'ref' => $_POST['ref'],
+                    'libelle' => $_POST['libelle'],
+                    'code_barre' => $_POST['code_barre'],
+                    'prix_achat' => $_POST['prix_achat'],
+                    'prix_final' => $_POST['prix_final'],
+                    'prix_offre' => $_POST['prix_offre'],
+                    'quantite' => $_POST['quantite'],
+                    'description' => $_POST['description'],
+                    'image' => $imgName,
+                    'categorie_id' => $_POST['categorie_id'],
+
                        
                 ];
                 $test=$this->productmodels->add($data);
@@ -50,10 +59,10 @@
                 'prix_offre' => '',
                 'quantite' => '',
                 'description' => '',
-                'photo' => '',
+                'image' => '',
                 'categorie_id' => '',
             ];
-            // $this->view('pages/addproduct',$data);
+           $this->view('pages/admin/add/addproduct',$data);
         }
         }
         public function update(){
@@ -71,7 +80,7 @@
                     'prix_offre' => $_POST['prix_offre'],
                     'quantite' => $_POST['quantite'],
                     'description' => $_POST['description'],
-                    'photo' => $_FILES['photo'],
+                    'image' => $_FILES['image'],
                     'categorie_id' => $_POST['categorie_id'],
                 ];
                 $test=$this->productmodels->update($data);
@@ -94,11 +103,23 @@
                     'prix_offre' => '',
                     'quantite' => '',
                     'description' => '',
-                    'photo' => '',
+                    'image' => '',
                     'categorie_id' => '',
                 ];
                 $this->view('pages/updateproduct',$data);
         }
         }
-        
+    public function delete($id)
+    {
+        if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+
+            if ($this->productmodels->Delete($id)) {
+                redirect('Products/dashboard');
+            } else {
+                die('Something went wrong');
+            }
+        } else {
+            redirect('Products/dashboard');
+        }
+    }
     }
